04552d4573e0f618c9a7f9769c234b164394a335
ddcw <ddcw@qq.com>
2020-07-10 17:37:10 +0800

    third commit

---
+++ shells/ddcw.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+#write by ddcw 20200710 first
+#this script is for manage other shell scripts ,
+#it's have add , delete , update ,rollback commands (scripts) 
+#and its can used some default theme like PS1
+
+
